77323a95e620209025ec987f395106c8866b1c1f
Yuriy Larin <blandger@gmail.com>
2023-04-24 16:03:35 +0300

    added error fix and description

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # weldTestParametrized
 
-project is created as minimal example to show the problem using Weld junit5 library with ParameterizedTest junit5 annotation.
+The project is created as minimal example to show the problem using Weld junit5 library with **@ParameterizedTest** junit5 annotation.
 
 Run ServiceExampleTest and see failed doesntWork() test.
+
+In order to fix it you should use **@ExplicitParamInjection** annotation on test class.
 
--- src/test/java/org/weld/test/example/ServiceExampleTest.java
+++ src/test/java/org/weld/test/example/ServiceExampleTest.java
@@ -2,6 +2,7 @@ package org.weld.test.example;
 
 import jakarta.inject.Inject;
 import org.jboss.weld.junit5.EnableWeld;
+import org.jboss.weld.junit5.ExplicitParamInjection;
 import org.jboss.weld.junit5.WeldInitiator;
 import org.jboss.weld.junit5.WeldSetup;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @EnableWeld
+@ExplicitParamInjection // that line fixes error like ParameterResolutionException: Weld has failed to resolve test parameter...
 class ServiceExampleTest {
 
     @Inject
@@ -26,8 +28,14 @@ class ServiceExampleTest {
     // org.junit.jupiter.api.extension.ParameterResolutionException: Weld has failed to resolve test parameter [java.lang.String arg0] in method [void org.weld.test.example.ServiceExampleTest.callSomething2(java.lang.String)].
     // Unsatisfied dependency has type java.lang.String and qualifiers [].
     @ParameterizedTest
-    @ValueSource(strings = {"2", "Deal2"})
+    @ValueSource(strings = {"1", "Deal1"})
     void doesntWork(String value) {
+        serviceExample.callSomething(value);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "Deal2"})
+    void doesntWork2(String value) {
         serviceExample.callSomething(value);
     }
 
